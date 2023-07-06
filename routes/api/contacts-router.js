3e685b9f080ed5067bb6dd0a5ececd03c5573d72
HollyUndead <57831657+HollyUndead@users.noreply.github.com>
2023-07-06 17:25:54 +0300

    fix

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -5,13 +5,12 @@ const {
   removeContact,
   addContact,
   updateContact,
+  changeFavorite
 } = require("../../controller/contacts-controller.js");
-const {isValidId, isBodyEmpty, validateBody, authentificate} = require('../../middlewares/index.js')
-const {contactSchemaJoi} = require('../../schema/schemaJOI.js')
+const {isValidId, isBodyEmpty,validateBody} = require('../../middlewares/index.js')
+const contactSchema = require('../../schema/contact-schemaJOI.js')
 
 const router = express.Router();
-
-router.use(authentificate)
 
 router.get("/", listContacts)
 
@@ -19,13 +18,19 @@ router.get("/", listContacts)
 router.get("/:contactId", isValidId, getContactById)
 
 
-router.post("/", isBodyEmpty, validateBody(contactSchemaJoi), addContact)
+router.post("/", isBodyEmpty, validateBody(contactSchema), addContact)
 
 
 router.delete("/:contactId", isValidId, removeContact)
 
 
-router.put("/:contactId", isValidId, isBodyEmpty, validateBody(contactSchemaJoi), updateContact)
+router.put("/:contactId", isValidId, isBodyEmpty, validateBody(contactSchema), updateContact)
+
+
+router.patch('/:contactId/favorite', isValidId, isBodyEmpty, validateBody(contactSchema), changeFavorite)
+
+
+router.patch('/:contactId/favorite', isValidId, isBodyEmpty, validateBody(contactSchema), changeFavorite)
 
 
 
