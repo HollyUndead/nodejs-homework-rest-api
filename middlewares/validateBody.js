45f8698e7a1868e1df873f396e1d537d81ac5805
HollyUndead <57831657+HollyUndead@users.noreply.github.com>
2023-07-06 17:04:03 +0300

    fix

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -4,7 +4,12 @@ const validateBody = schema => {
     const func = (req, res, next) => {
         const { error } = schema.validate(req.body);
         if (error) {
-            next(HttpError(400, error.message))
+            if(error.details[0].message.split(' ').includes('contain')){
+                next(HttpError(400, 'Помилка від Joi або іншої бібліотеки валідації'))
+            }
+            if(error.details[0].message.split(' ').includes('required')){
+                next(HttpError(400, `missing required ${error.details[0].path[0]} field`))
+            }
         }
         next();
     }
